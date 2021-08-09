93309b3183a77f066d6739a2a868f317310ae1ab
ElayyanAreej <areej.hossein@gmail.com>
2021-08-10 00:39:35 +0300

    fixing

--- javascript/code-challenges/challenges-02.test.js
+++ javascript/code-challenges/challenges-02.test.js
@@ -58,10 +58,15 @@ For example, twoToThe([1,2,3]) returns [2,4,8] because 2 ^ 1 = 2, 2 ^ 2 = 4, and
 
 const forLoopTwoToThe = (arr) => {
   // Solution code here...
-  let newArry=arr.map((item)=>{
-    return Math.pow(2,item);
-  });
+  let newArry=[];
+  for(let i=0;i<arr.length;i++)
+    newArry.push(Math.pow(2,arr[i]));
+
   return newArry;
+  // let newArry=arr.map((item)=>{
+  //   return Math.pow(2,item);
+  // });
+  // return newArry;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -87,8 +92,9 @@ Write a function named mapTwoToThe that produces the same output as your forLoop
 
 const mapTwoToThe = (arr) => {
   // Solution code here...
-  let newArry=arr.map((item)=>{
-    return Math.pow(2,item);
+  let newArry=[];
+  arr.forEach((item)=>{
+    newArry.push(Math.pow(2,item));
   });
   return newArry;
 };
